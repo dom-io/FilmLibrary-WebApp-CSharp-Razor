ac2affa3d7f7fd313c41234735251e23588e3317
Dom <111844962+dom-io@users.noreply.github.com>
2023-03-13 19:36:38 -0700

    Added Movie.cs and updated MyList so inputs to add movies are on the page and have limited functionaility.

---
+++ FilmLibrary/Shared/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmLibrary.Shared
+{
+    public class Movie
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public int ReleaseYear { get; set; }
+    }
+}
