8bc5882c7e894931468a60f1d7e4428dff9c6ada
Dom <111844962+dom-io@users.noreply.github.com>
2023-03-23 19:51:28 -0700

    Added Login file for future.

---
+++ FilmLibrary/Shared/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmLibrary.Shared
+{
+    public class Login
+    {
+        // Future add on for user accounts
+    }
+}
